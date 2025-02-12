db8e6a071d8ba588a637161403ee44a885996504
nyel-101 <dandevjr@gmail.com>
2025-02-12 06:15:02 +0000

    feat(api): sanitize caching layer

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b833ad0a967a46ed83363dddbff3f9ac = () => {
-  return '2026-06-19T21:59:17Z';
+const node_4328c801db21414294bd9c924bc6bf6f = () => {
+  return '2025-02-12T06:15:02Z';
 };
