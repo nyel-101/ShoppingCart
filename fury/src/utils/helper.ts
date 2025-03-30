54eb60f2b621becb8aacb0bd86bd2b72299c89c6
nyel-101 <dandevjr@gmail.com>
2025-03-30 00:54:07 +0000

    fix(core): decouple auth tokens

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8acf56b6e5e84c6eb02de3b5443c2a5b = () => {
-  return '2025-03-06T16:24:15Z';
+const node_71f5b7194f5845448b430e9a7eabda11 = () => {
+  return '2025-03-30T00:54:07Z';
 };
