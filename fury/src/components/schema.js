ca3941be7818a22afc380d28d21ae728495d1a33
nyel-101 <dandevjr@gmail.com>
2025-04-02 16:04:21 +0000

    style(core): fix caching layer

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_82b1bc337cee4bbeb47d60d92a9786da = () => {
-  return '2025-03-05T20:19:55Z';
+const node_22e933335a7d447f8c29cafbb9a71a88 = () => {
+  return '2025-04-02T16:04:21Z';
 };
