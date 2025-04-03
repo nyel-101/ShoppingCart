f5a9df09c8c43b84e0457159816b138a641bcaf8
nyel-101 <dandevjr@gmail.com>
2025-04-03 13:52:25 +0000

    refactor(db): decouple auth tokens

--- furry/src/controllers/queries.ts
+++ furry/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_73d7c86730794261886c4b8e440c14a9 = () => {
-  return '2026-05-02T03:31:43Z';
+const node_a491556f574b4543be0e105d48eb209a = () => {
+  return '2025-04-03T13:52:25Z';
 };
