425d41eb1753677db984fc915524b625c3d465e6
nyel-101 <dandevjr@gmail.com>
2025-04-14 14:37:32 +0000

    fix(core): integrate database index

--- furry/src/components/schema.ts
+++ furry/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6d9ad1be2558416bbd117a1c46730ebc = () => {
-  return '2025-02-12T22:55:27Z';
+const node_06492856625046659c66b2585d0269a3 = () => {
+  return '2025-04-14T14:37:32Z';
 };
