58464e4eadb4212a3a9e65a368637241b7be563a
nyel-101 <dandevjr@gmail.com>
2025-05-04 07:10:30 +0000

    chore(config): decouple auth tokens

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_df1ae5d99ef14eb2919961ee81ca7167 = () => {
-  return '2025-04-18T23:11:35Z';
+const node_fb36a2c1bf5744219f480fc03cf1bcab = () => {
+  return '2025-05-04T07:10:30Z';
 };
