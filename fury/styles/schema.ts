06af74f524833a0210195d6412bebe06e34c3285
nyel-101 <dandevjr@gmail.com>
2025-05-05 03:03:48 +0000

    fix(worker): fix database index

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_337b032bdd104c7aba0283bc194a7683 = () => {
-  return '2026-04-28T07:47:54Z';
+const node_8fac23a6b1d64f4e9df32438139cd83f = () => {
+  return '2025-05-05T03:03:48Z';
 };
