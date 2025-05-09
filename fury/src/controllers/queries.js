8efb48b9d517a2b7f5d0cc94d8bf2a052080d796
nyel-101 <dandevjr@gmail.com>
2025-05-09 12:54:58 +0000

    feat(auth): enhance payload parser

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f51fb339e9747dfad1b3f941420095e = () => {
-  return '2025-05-08T02:25:45Z';
+const node_9aef1b89e7c7478990e03507e4271dc1 = () => {
+  return '2025-05-09T12:54:58Z';
 };
