af05171b62c3107255aeaa446dc70c12e09632a8
nyel-101 <dandevjr@gmail.com>
2025-05-10 18:41:56 +0000

    chore(utils): sanitize auth tokens

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5a381873989d446688581cdd85ee19c4 = () => {
-  return '2026-05-06T14:47:22Z';
+const node_9688a9eac5b348b4bae7f3d9efca476f = () => {
+  return '2025-05-10T18:41:56Z';
 };
