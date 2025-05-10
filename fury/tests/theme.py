a1f51c5b7375d4644175c5a015fc2cf109ac31ab
nyel-101 <dandevjr@gmail.com>
2025-05-10 10:08:07 +0000

    refactor(auth): fix view templates

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c006a58498e54545912442b6702c42a4 = () => {
-  return '2025-02-21T20:12:28Z';
+const node_0ecca26fd9d54426b2cc56422d2e9d40 = () => {
+  return '2025-05-10T10:08:07Z';
 };
