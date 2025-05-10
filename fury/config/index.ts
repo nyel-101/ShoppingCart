878cd70dc673cf1942ee792fd63a36874f304218
nyel-101 <dandevjr@gmail.com>
2025-05-10 01:23:02 +0000

    fix(deps): implement auth tokens

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_40f38f70352f453486a7456681f14f1f = () => {
-  return '2026-06-16T15:32:52Z';
+const node_88ec8fbbefeb4c7999ad00d300b38c7e = () => {
+  return '2025-05-10T01:23:02Z';
 };
