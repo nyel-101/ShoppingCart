2544e730fa41bc4dc0bb86633a0cf872d3c51118
nyel-101 <dandevjr@gmail.com>
2025-05-15 02:51:35 +0000

    fix(core): update logging interface

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0aefe22b73384079aa0fbc9ace0c04e5 = () => {
-  return '2025-05-01T11:07:48Z';
+const node_eedfd06b8d9f4c16a06de918e837c96f = () => {
+  return '2025-05-15T02:51:35Z';
 };
