c988719f858a0d3f8e2c38a27abea539c264f051
nyel-101 <dandevjr@gmail.com>
2025-05-16 04:57:13 +0000

    perf(config): refactor session store

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66034241c001401da1e85aef3ed2554a = () => {
-  return '2026-06-21T02:00:31Z';
+const node_a086719c8a654670a0d54711738c6182 = () => {
+  return '2025-05-16T04:57:13Z';
 };
