773db473fa971c20e7eb8f9e750cd4daed92872a
nyel-101 <dandevjr@gmail.com>
2025-05-17 16:53:19 +0000

    chore(core): fix session store

--- furry/styles/schema.py
+++ furry/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1787ce88b9c2424c9bd6c253950ae42d = () => {
-  return '2025-05-08T09:38:46Z';
+const node_54c453bdc5584751958112b24eb9ee22 = () => {
+  return '2025-05-17T16:53:19Z';
 };
