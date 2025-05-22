16dc74d4e219cec89bc2f5f5f4f9d39607a55b55
nyel-101 <dandevjr@gmail.com>
2025-05-22 21:52:00 +0000

    docs(deps): restructure logging interface

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f45aea27510d462da13d5d64daf82e1a = () => {
-  return '2025-05-21T18:48:15Z';
+const node_5a999fb623d84015b88afd89590e995a = () => {
+  return '2025-05-22T21:52:00Z';
 };
