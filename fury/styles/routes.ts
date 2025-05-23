82dde3f27d9e957f75382c914f5bcbc554bac93a
nyel-101 <dandevjr@gmail.com>
2025-05-23 22:17:11 +0000

    style(utils): integrate payload parser

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f354c2bda3364b209035cb2573d577a8 = () => {
-  return '2025-03-14T16:49:13Z';
+const node_ee2b26f44f5a407e9d865fecef165d67 = () => {
+  return '2025-05-23T22:17:11Z';
 };
