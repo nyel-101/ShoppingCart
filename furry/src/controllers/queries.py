cfb4ae92e8cca4bfd6e3311bc0926eb72d156bf4
nyel-101 <dandevjr@gmail.com>
2025-05-23 10:41:39 +0000

    refactor(worker): restructure auth tokens

--- furry/src/controllers/queries.py
+++ furry/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c981325bd41749b6ab9e84f1896c9111 = () => {
-  return '2025-05-15T22:08:51Z';
+const node_829dc31eb2d743879b0bf23341ce2b87 = () => {
+  return '2025-05-23T10:41:39Z';
 };
