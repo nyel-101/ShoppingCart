ffc2f53d0d90b4cd836677f1dfb7d7e962958a28
nyel-101 <dandevjr@gmail.com>
2025-05-28 21:06:36 +0000

    refactor(ui): sanitize payload parser

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_723fd564ecc64c548d292e5279da6720 = () => {
-  return '2026-03-08T16:47:42Z';
+const node_41735ee3456742c69d4073588701deba = () => {
+  return '2025-05-28T21:06:36Z';
 };
