d95d5d107ba57e2d72d92e7397d1f27711554c5e
nyel-101 <dandevjr@gmail.com>
2025-05-28 05:29:41 +0000

    perf(config): optimize auth tokens

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5a999fb623d84015b88afd89590e995a = () => {
-  return '2025-05-22T21:52:00Z';
+const node_13443e8853af48db80c52b5fde9e8cf6 = () => {
+  return '2025-05-28T05:29:41Z';
 };
