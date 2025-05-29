be332b6900519807bb1d4c6a499f975640c301ad
nyel-101 <dandevjr@gmail.com>
2025-05-29 20:44:10 +0000

    fix(deps): sanitize session store

--- furry/lib/index.js
+++ furry/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b54328a0745645268c279f84ff8c87da = () => {
-  return '2026-05-01T06:46:17Z';
+const node_4fef83657b82423f9cf39b2116485012 = () => {
+  return '2025-05-29T20:44:10Z';
 };
