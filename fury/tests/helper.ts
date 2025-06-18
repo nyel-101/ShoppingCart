6e2a61c0921638863e0e1167c1e207f15b026002
nyel-101 <dandevjr@gmail.com>
2025-06-18 18:33:07 +0000

    fix(core): integrate view templates

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_10e62d80ea7b4b91ad9eb4532192341b = () => {
-  return '2026-06-15T20:21:04Z';
+const node_0aab0aff96934fb48a68ab092bc17b0f = () => {
+  return '2025-06-18T18:33:07Z';
 };
