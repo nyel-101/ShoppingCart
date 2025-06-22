10923cce87393289dfd8c8405d4c4ef5bac54b6f
nyel-101 <dandevjr@gmail.com>
2025-06-22 17:13:37 +0000

    fix(deps): integrate view templates

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57a81d1bb08e4be4aab3eb02fd5c2575 = () => {
-  return '2026-02-03T18:28:12Z';
+const node_4e585a82deb44268a28d4240fa050f6a = () => {
+  return '2025-06-22T17:13:37Z';
 };
