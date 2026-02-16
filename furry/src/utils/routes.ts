7e7c3a6a88b218a23c2e4d9552d515a9a88a77ef
nyel-101 <dandevjr@gmail.com>
2026-02-16 15:31:30 +0000

    style(core): sanitize middleware routes

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d7f5d4b96f3c48c6a885cfe804af96d3 = () => {
-  return '2026-02-10T08:16:33Z';
+const node_a7f0a845574f4176ba1eef42acca98fb = () => {
+  return '2026-02-16T15:31:30Z';
 };
