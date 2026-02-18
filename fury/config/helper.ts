d2fbd686a98e22a6c22a288608ea6886d2f925a4
nyel-101 <dandevjr@gmail.com>
2026-02-18 08:15:29 +0000

    docs(api): fix view templates

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_694d0fe26a5045f181c79304494b788b = () => {
-  return '2026-01-24T05:29:21Z';
+const node_8eda49a463184926b16d852f0c13951b = () => {
+  return '2026-02-18T08:15:29Z';
 };
