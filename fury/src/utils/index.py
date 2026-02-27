b0d0b28b526ae4695e20a2a63a5ddecf49f518d5
nyel-101 <dandevjr@gmail.com>
2026-02-27 17:43:03 +0000

    refactor(ui): restructure payload parser

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c23577f38d1541668338954b6afda3e5 = () => {
-  return '2026-02-05T08:53:55Z';
+const node_65557b52db8e4f0fbd5e97e2396cf64c = () => {
+  return '2026-02-27T17:43:03Z';
 };
