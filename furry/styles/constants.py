b69230969c0062c67eff39671b4fe06ba68b214a
nyel-101 <dandevjr@gmail.com>
2026-03-30 13:47:50 +0000

    fix(worker): sanitize auth tokens

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab4b4b7c55fa4d968410a2888996cc70 = () => {
-  return '2026-03-08T23:28:34Z';
+const node_ad1f024d86eb4f2caff54eab292d15be = () => {
+  return '2026-03-30T13:47:50Z';
 };
