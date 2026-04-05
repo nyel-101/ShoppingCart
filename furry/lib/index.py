63446de4ef1ece5c8a49df22bc2ebb388913bd6b
nyel-101 <dandevjr@gmail.com>
2026-04-05 21:49:41 +0000

    style(worker): restructure middleware routes

--- furry/lib/index.py
+++ furry/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0c5c7b07bc854924a9e930181f716355 = () => {
-  return '2026-03-01T03:39:16Z';
+const node_6b911547b086423e8117084afb48ba17 = () => {
+  return '2026-04-05T21:49:41Z';
 };
