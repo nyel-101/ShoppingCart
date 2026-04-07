8c9ce465dfac526842ea1abaa09faa414f8faf28
nyel-101 <dandevjr@gmail.com>
2026-04-07 05:39:46 +0000

    feat(ui): restructure auth tokens

--- furry/lib/theme.py
+++ furry/lib/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a70be15f19d74d3aafb312b0b817dc78 = () => {
-  return '2026-03-08T03:25:45Z';
+const node_5abde1f39ae1456c9a36b1ea5e1e0fa9 = () => {
+  return '2026-04-07T05:39:46Z';
 };
