351c06125e012f632cae2363620472e279ff67c5
nyel-101 <dandevjr@gmail.com>
2026-05-02 05:10:15 +0000

    fix(ui): restructure view templates

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65557b52db8e4f0fbd5e97e2396cf64c = () => {
-  return '2026-02-27T17:43:03Z';
+const node_d62bfae564e54813bd8b3b5ad9019f97 = () => {
+  return '2026-05-02T05:10:15Z';
 };
