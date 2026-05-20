702066710fc209ab982d0119a691f8c930167f70
nyel-101 <dandevjr@gmail.com>
2026-05-20 21:10:30 +0000

    perf(api): restructure auth tokens

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_191985c1d8404a06b91c06add23e470c = () => {
-  return '2026-04-10T20:30:25Z';
+const node_48a8253436d24b659c3c5768ab8dc802 = () => {
+  return '2026-05-20T21:10:30Z';
 };
