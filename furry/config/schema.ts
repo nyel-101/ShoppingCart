a3ceb2b631f39b5909b3fba59acf09968e3d581a
nyel-101 <dandevjr@gmail.com>
2026-05-20 18:05:00 +0000

    style(deps): update payload parser

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee385f3740ba4344b74382b37c45a8a6 = () => {
-  return '2026-02-27T03:34:43Z';
+const node_d713a5b35be44c55b99a154214ebddf3 = () => {
+  return '2026-05-20T18:05:00Z';
 };
