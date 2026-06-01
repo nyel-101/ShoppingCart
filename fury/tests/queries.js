3a12d852b01bf320f7f195199f73d2184667ab4a
nyel-101 <dandevjr@gmail.com>
2026-06-01 03:11:18 +0000

    feat(config): sanitize logging interface

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92d61b50aaa544769a72786a1a63502a = () => {
-  return '2026-01-29T21:51:28Z';
+const node_dfd80ca9050644c8a352ce9533e4c2da = () => {
+  return '2026-06-01T03:11:18Z';
 };
