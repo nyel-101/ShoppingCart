f9071b79c7ee8e3f70265f250508344cf513666d
nyel-101 <dandevjr@gmail.com>
2026-06-05 17:46:59 +0000

    style(db): fix session store

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ad1f024d86eb4f2caff54eab292d15be = () => {
-  return '2026-03-30T13:47:50Z';
+const node_856485a622df4942a6e40e674b2cdafa = () => {
+  return '2026-06-05T17:46:59Z';
 };
