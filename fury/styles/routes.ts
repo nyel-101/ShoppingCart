7ec5cd8799fd65778fc7e02efdac1d439c471b2a
nyel-101 <dandevjr@gmail.com>
2026-06-05 19:26:05 +0000

    fix(api): sanitize payload parser

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e86bec2c98d34049bee19eb2a431c58f = () => {
-  return '2026-03-20T22:34:59Z';
+const node_0e10ffeb75694fdca76264ec5428b998 = () => {
+  return '2026-06-05T19:26:05Z';
 };
