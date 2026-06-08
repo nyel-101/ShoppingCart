af1208a1a7bf8781d0cbbf48f0e819b35ffb88fe
nyel-101 <dandevjr@gmail.com>
2026-06-08 11:37:00 +0000

    chore(ui): optimize middleware routes

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c4d2f1bf7f094dc58b7001c28ae01af2 = () => {
-  return '2026-01-06T01:19:17Z';
+const node_22e64973947c467f8d8a1ec57e409a67 = () => {
+  return '2026-06-08T11:37:00Z';
 };
