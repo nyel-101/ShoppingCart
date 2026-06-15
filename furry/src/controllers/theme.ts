188fd894bdee25e04e9496cc36bb0e7297d15339
nyel-101 <dandevjr@gmail.com>
2026-06-15 16:35:21 +0000

    fix(auth): restructure session store

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a2b2b9994d344cd696f05db4bfdc4e4a = () => {
-  return '2026-04-30T23:33:45Z';
+const node_f980d01b267146449443794009115239 = () => {
+  return '2026-06-15T16:35:21Z';
 };
