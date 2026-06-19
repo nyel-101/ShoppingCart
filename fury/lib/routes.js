1c796b4e13d04d3ce5d695675430f3d84106d9c8
nyel-101 <dandevjr@gmail.com>
2026-06-19 21:59:17 +0000

    fix(worker): fix session store

--- fury/lib/routes.js
+++ fury/lib/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ebe6acfdd067459990863d8ada0e35d7 = () => {
-  return '2026-01-03T15:01:58Z';
+const node_b833ad0a967a46ed83363dddbff3f9ac = () => {
+  return '2026-06-19T21:59:17Z';
 };
